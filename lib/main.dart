69415b1347c46dbd82384279a2b8b744f98e93e0
Swaraj961 <swarajroutray961@gmail.com>
2020-05-18 09:26:35 +0530

    adding a change from the feature branch

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoadingScreen(),
-    );
+      home:LoadingScreen()
+      );
+    
   }
 }
